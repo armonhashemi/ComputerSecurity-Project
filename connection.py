bceb62e1b192060e69ca1025f0d1a9cd591e29be
armonhashemi <armon.aslehashemi@gmail.com>
2023-04-25 17:57:44 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -9,11 +9,27 @@ def server(shared_key):
     client_socket, addr = server_socket.accept()
     print("Connection from:", addr)
 
-    while True:
-        data = client_socket.recv(1024)
-        if not data: break
-        decrypted_message = decrypt_message(data, shared_key)
-        print("Received:", decrypted_message)
+    def receive_messages():
+        while True:
+            data = client_socket.recv(1024)
+            if not data: break
+            decrypted_message = decrypt_message(data, shared_key)
+            print("Received:", decrypted_message)
+
+    def send_messages():
+        while True:
+            message = input("Server: Enter message: ")
+            encrypted_message = encrypt_message(message, shared_key)
+            client_socket.send(encrypted_message)
+
+    receive_thread = threading.Thread(target=receive_messages)
+    send_thread = threading.Thread(target=send_messages)
+
+    receive_thread.start()
+    send_thread.start()
+
+    receive_thread.join()
+    send_thread.join()
 
     client_socket.close()
 
@@ -21,9 +37,19 @@ def client(shared_key):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 12345))
 
-    while True:
-        message = input("Enter message: ")
-        encrypted_message = encrypt_message(message, shared_key)
-        client_socket.send(encrypted_message)
+    def receive_messages():
+        while True:
+            data = client_socket.recv(1024)
+            if not data: break
+            decrypted_message = decrypt_message(data, shared_key)
+            print("Received:", decrypted_message)
 
-    client_socket.close()
+    def send_messages():
+        while True:
+            message = input("Client: Enter message: ")
+            encrypted_message = encrypt_message(message, shared_key)
+            client_socket.send(encrypted_message)
+
+    receive_thread = threading.Thread(target=receive_messages)
+    send_thread = threading.Thread(target=send
+
