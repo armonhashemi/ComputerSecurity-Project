8b2d10c81ceb619e03c02fa08dad73fcf1be1e9f
armonhashemi <armon.aslehashemi@gmail.com>
2023-04-26 14:55:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from key_exchange import generate_shared_key
+from key_exchange import generate_key_pair, derive_shared_key
 from connection import server, client
 
 if __name__ == "__main__":
-    shared_key = generate_shared_key()
-    print("Shared key:", shared_key)
-
     choice = input("Choose (s)erver or (c)lient: ")
 
     if choice.lower() == "s":
-        server(shared_key)
+        server()
     elif choice.lower() == "c":
-        client(shared_key)
+        client()
