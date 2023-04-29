46c493162b7f7414bb848a2b9e5c0dd0ae666dc1
Marc Hanna <Marc.Hanna-1@ou.edu>
2023-04-29 17:21:08 -0500

    New shared key update

--- connection.py
+++ connection.py
@@ -6,6 +6,27 @@ from encryption import encrypt_message, decrypt_message
 from key_exchange import generate_key_pair, derive_shared_key
 
 
+import socket
+import threading
+import time
+
+from encryption import encrypt_message, decrypt_message
+from key_exchange import generate_key_pair, derive_shared_key
+
+
+def key_update(client_socket, shared_key, role):
+    while True:
+        time.sleep(30)  # Update key every 30 seconds
+        key_pair = generate_key_pair()
+        pubkey = key_pair.gen_public_key()
+        client_socket.send(pubkey.to_bytes(4096, 'big'))
+        remote_pubkey_bytes = client_socket.recv(4096)
+        remote_pubkey = int.from_bytes(remote_pubkey_bytes, 'big')
+
+        shared_key[0] = derive_shared_key(key_pair, remote_pubkey)
+        print(f"{role}: new shared_key: ", shared_key[0])
+
+
 def server(ip_address, port_num):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((ip_address, port_num))
@@ -20,43 +41,33 @@ def server(ip_address, port_num):
     client_pubkey_bytes = client_socket.recv(4096)
     client_pubkey = int.from_bytes(client_pubkey_bytes, 'big')
 
-    shared_key = derive_shared_key(server_key_pair, client_pubkey)
+    shared_key = [derive_shared_key(server_key_pair, client_pubkey)]
 
     def receive_messages():
         while True:
             data = client_socket.recv(4096)
             if not data: break
-            decrypted_message = decrypt_message(data, shared_key)
+            decrypted_message = decrypt_message(data, shared_key[0])
             print('\n' + "Received:", decrypted_message)
 
     def send_messages():
         while True:
             prefix = "Server: Enter message: "
             message = input(prefix)
-            encrypted_message = encrypt_message(message, shared_key)
+            encrypted_message = encrypt_message(message, shared_key[0])
             client_socket.send(encrypted_message)
 
     receive_thread = threading.Thread(target=receive_messages)
     send_thread = threading.Thread(target=send_messages)
+    key_update_thread = threading.Thread(target=key_update, args=(client_socket, shared_key, "Server"))
 
     receive_thread.start()
     send_thread.start()
-
-    # Key update timer
-    while True:
-        # TODO modify this to be every 10 minutes
-        time.sleep(30)  # 10 minutes
-        server_key_pair = generate_key_pair()
-        server_pubkey = server_key_pair.gen_public_key()
-        client_socket.send(server_pubkey.to_bytes(4096, 'big'))
-        client_pubkey_bytes = client_socket.recv(4096)
-        client_pubkey = int.from_bytes(client_pubkey_bytes, 'big')
-
-        shared_key = derive_shared_key(server_key_pair, client_pubkey)
-        print("new shared_key: ", shared_key)
+    key_update_thread.start()
 
     receive_thread.join()
     send_thread.join()
+    key_update_thread.join()
 
     client_socket.close()
 
@@ -72,42 +83,32 @@ def client(ip_address, port_num):
     server_pubkey = int.from_bytes(server_pubkey_bytes, 'big')
     client_socket.send(client_pubkey.to_bytes(4096, 'big'))
 
-    shared_key = derive_shared_key(client_key_pair, server_pubkey)
+    shared_key = [derive_shared_key(client_key_pair, server_pubkey)]
 
     def receive_messages():
         while True:
             data = client_socket.recv(4096)
             if not data: break
-            decrypted_message = decrypt_message(data, shared_key)
+            decrypted_message = decrypt_message(data, shared_key[0])
             print('\n' + "Received:", decrypted_message)
 
     def send_messages():
         while True:
             prefix = "Client: Enter message: "
             message = input(prefix)
-            encrypted_message = encrypt_message(message, shared_key)
+            encrypted_message = encrypt_message(message, shared_key[0])
             client_socket.send(encrypted_message)
 
     receive_thread = threading.Thread(target=receive_messages)
     send_thread = threading.Thread(target=send_messages)
+    key_update_thread = threading.Thread(target=key_update, args=(client_socket, shared_key, "Client"))
 
     receive_thread.start()
     send_thread.start()
-
-    # Key update timer
-    while True:
-        # TODO modify this to be every 10 minutes
-        time.sleep(30)  # 10 minutes
-        client_key_pair = generate_key_pair()
-        client_pubkey = client_key_pair.gen_public_key()
-        client_socket.send(client_pubkey.to_bytes(4096, 'big'))
-        server_pubkey_bytes = client_socket.recv(4096)
-        server_pubkey = int.from_bytes(server_pubkey_bytes, 'big')
-
-        shared_key = derive_shared_key(client_key_pair, server_pubkey)
-        print("new shared_key: ", shared_key)
+    key_update_thread.start()
 
     receive_thread.join()
     send_thread.join()
+    key_update_thread.join()
 
     client_socket.close()
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from connection import server, client
 if __name__ == "__main__":
     choice = input("Choose (s)erver or (c)lient: ")
 
-    ip_address = '10.204.133.222'
-    port = 12349
+    ip_address = '10.190.240.93'
+    port = 12300
     if choice.lower() == "s":
         server(ip_address, port)
     elif choice.lower() == "c":
