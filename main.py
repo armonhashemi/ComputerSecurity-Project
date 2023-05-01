79f43ec35695d42449aa9a39e5948b028a7d32f1
Anthony Immenschuh <anthonyimmenschuh@Anthonys-MacBook-Pro.local>
2023-04-30 22:12:49 -0500

    ui is done

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from connection import server, client
 if __name__ == "__main__":
     choice = input("Choose (s)erver or (c)lient: ")
 
-    ip_address = '10.5.15.233'
-    port = 12340
+    ip_address = '127.0.0.1'
+    port = 50003
     if choice.lower() == "s":
         server(ip_address, port)
-
     elif choice.lower() == "c":
         client(ip_address, port)
 
